8d8aa65dd1f43e0ab9ca28e338e82a8e5783c562
Jeromy Evans (personal) <jeromy.evans@blueskyminds.com.au>
2015-01-20 23:09:05 +1100

    Initial set up of empty project plus base images for maze project

---
+++ Maze/Maze/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maze
+//
+//  Created by Jeromy Evans (personal) on 20/01/2015.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-maze-demo/ios-maze-demo/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  ios-maze-demo
+//
+//  Created by Jeromy Evans (personal) on 20/01/2015.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
+
